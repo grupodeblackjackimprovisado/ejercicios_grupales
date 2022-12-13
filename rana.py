8006755a850b4304d101c30d461a6508473d012a
mgonzalz <mgonzgarc@myuax.com>
2022-12-14 00:10:40 +0100

    codigo

---
+++ rana.py
@@ -0,0 +1,21 @@
+import math
+import os
+import random
+import re
+import sys
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+    n = int(first_multiple_input[0])
+    m = int(first_multiple_input[1])
+    k = int(first_multiple_input[2])
+    for n_itr in range(n):
+        row = input()
+# Write your code here
+    for k_itr in range(k):
+        second_multiple_input = input().rstrip().split()
+        i1 = int(second_multiple_input[0])
+        j1 = int(second_multiple_input[1])
+        i2 = int(second_multiple_input[2])
+        j2 = int(second_multiple_input[3])
+# Write your code here
+# Write your code here
