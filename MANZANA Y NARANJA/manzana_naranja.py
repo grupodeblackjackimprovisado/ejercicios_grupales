b9f3c36f00ac2846330562309b8e489e62df48c4
Jacobo24 <jaccalgar@gmail.com>
2022-12-16 10:14:16 +0100

    cambios

--- MANZANA Y NARANJA/manzana_naranja.py
+++ MANZANA Y NARANJA/manzana_naranja.py
@@ -19,14 +19,14 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
 # Write your code here
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
-    s = int(first_multiple_input[0])
-    t = int(first_multiple_input[1])
+    s = int(first_multiple_input[0]) # inicio de la casa
+    t = int(first_multiple_input[1]) # fin de la casa
     second_multiple_input = input().rstrip().split()
-    a = int(second_multiple_input[0])
-    b = int(second_multiple_input[1])
+    a = int(second_multiple_input[0]) # posicion de la manzana
+    b = int(second_multiple_input[1]) # posicion de la naranja
     third_multiple_input = input().rstrip().split()
-    m = int(third_multiple_input[0])
-    n = int(third_multiple_input[1])
+    m = int(third_multiple_input[0]) # numero de manzanas
+    n = int(third_multiple_input[1]) # numero de naranjas
     apples = list(map(int, input().rstrip().split()))
     oranges = list(map(int, input().rstrip().split()))
     countApplesAndOranges(s, t, a, b, apples, oranges)
