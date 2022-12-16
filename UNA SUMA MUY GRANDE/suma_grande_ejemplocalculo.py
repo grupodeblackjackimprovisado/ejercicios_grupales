9443002916969504a79b71a86937b1f1a3dd66d6
mgonzalz <mgonzgarc@myuax.com>
2022-12-16 09:31:28 +0100

    corrección

--- UNA SUMA MUY GRANDE/suma_grande_ejemplocalculo.py
+++ UNA SUMA MUY GRANDE/suma_grande_ejemplocalculo.py
@@ -6,12 +6,16 @@ import sys
 
 def aVeryBigSum(ar):
     suma = 0
-    for i in range(len(ar)):
-        suma = suma + ar[i]
+    for i in ar: # sirve para recorrer la matriz
+        for j in i: # sirve para recorrer cada elemento de la matriz
+            suma = suma + j
     print("El resultado de la suma de la siguiente matriz es de: ", suma)
 
 
 
 if __name__ == '__main__':
-    ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005, 1000000050, 1000000600]
+    ar = [
+        [1000000001, 1000000002, 1000000003, 1000000004, 1000000005, 1000000050, 1000000600], 
+        [1000000001, 1000000002, 1000000003, 1000000004, 1000000005, 1000000050, 1000000600],
+        [1000000001, 1000000002, 1000000003, 1000000004, 1000000005, 1000000050, 1000000600]]
     result = aVeryBigSum(ar)
