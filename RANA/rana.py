ac874669b5cb865d7df10946536ffd764acd1df1
mgonzalz <mgonzgarc@myuax.com>
2022-12-21 16:27:22 +0100

    rana prueba

--- RANA/rana.py
+++ RANA/rana.py
@@ -5,17 +5,18 @@ import re
 import sys
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
-    n = int(first_multiple_input[0])
-    m = int(first_multiple_input[1])
-    k = int(first_multiple_input[2])
+    n = int(first_multiple_input[0]) # row
+    m = int(first_multiple_input[1]) # column
+    k = int(first_multiple_input[2]) # number of queries
     for n_itr in range(n):
         row = input()
 # Write your code here
     for k_itr in range(k):
         second_multiple_input = input().rstrip().split()
-        i1 = int(second_multiple_input[0])
-        j1 = int(second_multiple_input[1])
-        i2 = int(second_multiple_input[2])
-        j2 = int(second_multiple_input[3])
+        i1 = int(second_multiple_input[0]) # row
+        j1 = int(second_multiple_input[1]) # column
+        i2 = int(second_multiple_input[2]) # row
+        j2 = int(second_multiple_input[3]) # column
 # Write your code here
 # Write your code here
+
