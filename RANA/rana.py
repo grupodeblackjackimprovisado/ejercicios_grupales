7d6389528ed239524baff28a4cfaca2fed58df90
Jacobo24 <jaccalgar@gmail.com>
2022-12-21 18:39:13 +0100

    comentarios rana

--- RANA/rana.py
+++ RANA/rana.py
@@ -5,18 +5,18 @@ import re
 import sys
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
-    n = int(first_multiple_input[0]) # row
-    m = int(first_multiple_input[1]) # column
-    k = int(first_multiple_input[2]) # number of queries
+    n = int(first_multiple_input[0]) # fila
+    m = int(first_multiple_input[1]) # columna
+    k = int(first_multiple_input[2]) # número de movimientos
     for n_itr in range(n):
         row = input()
 # Write your code here
     for k_itr in range(k):
         second_multiple_input = input().rstrip().split()
-        i1 = int(second_multiple_input[0]) # row
-        j1 = int(second_multiple_input[1]) # column
-        i2 = int(second_multiple_input[2]) # row
-        j2 = int(second_multiple_input[3]) # column
+        i1 = int(second_multiple_input[0]) # fila
+        j1 = int(second_multiple_input[1]) # columna
+        i2 = int(second_multiple_input[2]) # fila
+        j2 = int(second_multiple_input[3]) # columna
 # Write your code here
 # Write your code here
 
